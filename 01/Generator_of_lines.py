26b8d7afe2d207acd516318d725b68b0e0156e1b
Jimsgood <113968960+DmitriiKolesnikov@users.noreply.github.com>
2023-04-17 20:05:34 +0300

    Update Generator_of_lines.py

--- 01/Generator_of_lines.py
+++ 01/Generator_of_lines.py
@@ -6,18 +6,15 @@ def generator_of_lines(file_name_or_file, *finding_words):
         words.append(word.lower())
     if isinstance(file_name_or_file, str):
         with open(file_name_or_file, "r") as file:
-            print(type(file), type(words))
             for line in file:
-                yield line.strip()
                 if word.lower() in line.lower():
-                    print(line)
-                else:
-                    print("|||||| There is no such word as", "'", word, "'", 'in this line' "||||||")
-
+                    yield line.strip()
+    else:
         file = file_name_or_file
         for line in file:
-            yield line.strip()
             if word.lower() in line.lower():
-                print(line)
-            else:
-                print("|||||| There is no such word as", "'", word, "'", 'in this line' "||||||")
+                yield line.strip()
+
+
+if __name__ == "__main__":
+    pass
