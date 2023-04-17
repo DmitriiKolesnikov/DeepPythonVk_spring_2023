ee96e66117caa889eb51d08d884af1e13bb06c17
Jimsgood <113968960+DmitriiKolesnikov@users.noreply.github.com>
2023-04-17 20:06:39 +0300

    Update Homework_1_tests.py

--- 01/Homework_1_tests.py
+++ 01/Homework_1_tests.py
@@ -1,69 +1,6 @@
 from SomeModel import *
 from Generator_of_lines import *
-
-def what_is_in_the_predict(message):
-    res = type(message)
-    if res == int:
-        return f"Message type is int, it's the wrong type you need str"
-    elif res == float:
-        return f"Message type is float, it's the wrong type you need str"
-    elif res == str:
-        return f"Message type is str"
-    elif res == bool:
-        return f"Message type is bool, it's the wrong type you need str"
-    elif res == complex:
-        return f"Message type is complex, it's the wrong type you need str"
-    elif res == list:
-        return f"Message type is list, it's the wrong type you need str"
-    elif res == tuple:
-        return f"Message type is tuple, it's the wrong type you need str"
-    elif res == set:
-        return f"Message type is set, it's the wrong type you need str"
-    elif res == dict:
-        return f"Message type is dict, it's the wrong type you need str"
-    elif res ==frozenset:
-        return f"Message type is frozenset, it's the wrong type you need str"
-    else:
-        return f"it's the wrong type you need str"
-
-
-def check_end_cases(bad_thresholds: float = 0.3, good_thresholds: float = 0.8):
-
-    def custom_predict_message_mood(score):
-        if score < bad_thresholds:
-            print("неуд", score)
-        elif score == bad_thresholds:
-            print("норм, вы указали пограничный случай для нормальных показателей")
-        elif score > good_thresholds:
-            print("отл", score)
-        elif score == good_thresholds:
-            print("отл, вы указали пограничный случай для отличных показателей")
-        else:
-            print("норм", score)
-
-    assert custom_predict_message_mood(0.3) == "норм, вы указали пограничный случай для нормальных показателей", False
-    assert custom_predict_message_mood(0.8) == "отл, вы указали пограничный случай для отличных показателей", False
-
-
-def check_custom_end_cases():
-
-    def custom_predict_message_mood(score, bad_thresholds, good_thresholds):
-        if score < bad_thresholds:
-            print("неуд", score)
-        elif score == bad_thresholds:
-            print("норм, вы указали пограничный случай для нормальных показателей")
-        elif score > good_thresholds:
-            print("отл", score)
-        elif score == good_thresholds:
-            print("отл, вы указали пограничный случай для отличных показателей")
-        else:
-            print("норм", score)
-
-    assert custom_predict_message_mood(0.4, 0.4, 0.6) == "норм, вы указали пограничный случай для нормальных показателей", False
-    assert custom_predict_message_mood(0.4, 0.5, 0.6) == "неуд 0.4", False
-    assert custom_predict_message_mood(0.4, 0.1, 0.4) == "отл, вы указали пограничный случай для отличных показателей", False
-    assert custom_predict_message_mood(0.4, 0.3, 0.6) == "норм 0.4", False
-    assert custom_predict_message_mood(0.99, 0.8, 0.9) == "отл 0.99", False
+import unittest
 
 
 def test_second_task():
@@ -78,5 +15,70 @@ def test_second_task():
     assert res3 == "написать генератор filter_file для чтения и фильтрации файла", False
 
 
+class TestMessageMoodAndGeneratorOfLines(unittest.TestCase):
+
+    def test_predict_type_errors(self):
+        self.assertRaises(ValueError, SomeModel.predict, int)
+        self.assertRaises(ValueError, SomeModel.predict, float)
+        self.assertRaises(ValueError, SomeModel.predict, bool)
+        self.assertRaises(ValueError, SomeModel.predict, complex)
+        self.assertRaises(ValueError, SomeModel.predict, list)
+        self.assertRaises(ValueError, SomeModel.predict, tuple)
+        self.assertRaises(ValueError, SomeModel.predict, set)
+        self.assertRaises(ValueError, SomeModel.predict, dict)
+        self.assertRaises(ValueError, SomeModel.predict, frozenset)
+
+    def test_custom_end_cases(self):
+
+        def custom_predict_message_mood(score, bad_thresholds, good_thresholds):
+            if score < bad_thresholds:
+                return f"неуд {score}"
+            elif score == bad_thresholds:
+                return f"норм, вы указали пограничный случай для нормальных показателей"
+            elif score > good_thresholds:
+                return f"отл {score}"
+            elif score == good_thresholds:
+                return f"отл, вы указали пограничный случай для отличных показателей"
+            else:
+                return f"норм {score}"
+
+        custom_predict_message_mood(0.4, 0.4, 0.6)
+
+        self.assertEqual(custom_predict_message_mood(0.4, 0.4, 0.6).__str__(), "норм, вы указали пограничный случай для нормальных показателей")
+        self.assertEqual(custom_predict_message_mood(0.4, 0.5, 0.6).__str__(), "неуд 0.4")
+        self.assertEqual(custom_predict_message_mood(0.4, 0.1, 0.4).__str__(), "отл, вы указали пограничный случай для отличных показателей")
+        self.assertEqual(custom_predict_message_mood(0.4, 0.3, 0.6).__str__(), "норм 0.4")
+        self.assertEqual(custom_predict_message_mood(0.99, 0.8, 0.9).__str__(), "отл 0.99")
+
+    def test_end_cases(self):
+
+        def custom_predict_message_mood(score, bad_thresholds: float = 0.3, good_thresholds: float = 0.8):
+            if score < bad_thresholds:
+                return f"неуд {score}"
+            elif score == bad_thresholds:
+                return f"норм, вы указали пограничный случай для нормальных показателей"
+            elif score > good_thresholds:
+                return f"отл {score}"
+            elif score == good_thresholds:
+                return f"отл, вы указали пограничный случай для отличных показателей"
+            else:
+                return f"норм {score}"
+
+        self.assertEqual(custom_predict_message_mood(0.3).__str__(), "норм, вы указали пограничный случай для нормальных показателей")
+        self.assertEqual(custom_predict_message_mood(0.8).__str__(), "отл, вы указали пограничный случай для отличных показателей")
+
+    def test_generator(self):
+        s1 = generator_of_lines("Homework1Vk.txt", "в")
+        res = next(s1)
+        s2 = generator_of_lines("Homework1Vk.txt", "файла")
+        res2 = next(s2)
+        s3 = generator_of_lines("Homework1Vk.txt", "файла", "не")
+        res3 = next(s3)
+        self.assertEqual(res, "Есть текстовый файл который может не помещаться в память")
+        self.assertEqual(res2, "Написать генератор filter_file для чтения и фильтрации файла")
+        self.assertEqual(res3, "Написать генератор filter_file для чтения и фильтрации файла")
+
+
 if __name__ == "__main__":
-    print("First homework")
+    unittest.main()
+
