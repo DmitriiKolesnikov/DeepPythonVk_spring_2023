17c50fdb61b966535bf7e8bd29d83de03ea08e14
Jimsgood <113968960+DmitriiKolesnikov@users.noreply.github.com>
2023-04-27 11:29:08 +0300

    Update Servert_test.py

--- 06/Servert_test.py
+++ 06/Servert_test.py
@@ -9,13 +9,13 @@ class TestServer(unittest.TestCase):
       server = MasterWorkerServer('localhost', 8000, 10, 7)
       client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       client_socket.connect(('localhost', 8000))
-      url = 'http://example.com'
+      url = 'https://ru.wikipedia.org/wiki/Baba_Yaga'
       server.dispatch_work(url, client_socket)
       assert server.task_queue.get() == (url, client_socket)
 
   def test_worker_loop():
       server = MasterWorkerServer('localhost', 8000, 10, 7)
-      url = 'http://example.com'
+      url = 'https://ru.wikipedia.org/wiki/Baba_Yaga'
       client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       server.task_queue.put((url, client_socket))
       server.worker_loop()
@@ -26,7 +26,7 @@ class TestServer(unittest.TestCase):
       server = MasterWorkerServer('localhost', 8000, 10, 7)
       t = threading.Thread(target=server.start)
       t.start()
-      time.sleep(1)  # wait for server to start
+      time.sleep(1)
       client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       client_socket.connect(('localhost', 8000))
       url = 'http://example.com'
