2cfaeab535009692bfefa478d6c606aa2adaa9f3
Jimsgood <113968960+DmitriiKolesnikov@users.noreply.github.com>
2023-04-27 11:09:19 +0300

    Update Server.py

--- 06/Server.py
+++ 06/Server.py
@@ -1,44 +1,64 @@
 import socket
 import threading
-import urllib.request
 import json
+import urllib.request
 from collections import Counter
 
 
-def worker(worker_id):
-    urls_processed = 0
-    while True:
-        try:
-            url, client_socket = master_queue.get()
-        except:
-            print(f"Worker {worker_id} finished. Processed {urls_processed} urls.")
-            return
-        try:
-            response = urllib.request.urlopen(url)
-            html = response.read().decode()
-            words = html.split()
-            top_k_words = dict(Counter(words).most_common(k))
-            json_data = json.dumps(top_k_words)
-            client_socket.sendall(json_data.encode())
-        except:
-            
-            client_socket.sendall(b"Error")
-        finally:
-            client_socket.close()
-            urls_processed += 1
-            print(f"Worker {worker_id} processed {url}. Total processed: {urls_processed}")
+class MasterWorkerServer:
+    def __init__(self, host, port, num_workers, top_k):
+        self.host = host
+        self.port = port
+        self.num_workers = num_workers
+        self.top_k = top_k
+        self.worker_threads = []
+        self.url_counter = 0
 
+    def start(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((self.host, self.port))
+        self.socket.listen(1)
+        print(f"Server listening on {self.host}:{self.port} with {self.num_workers} workers.")
 
-master_queue = queue.Queue()
-num_workers = 4
-for i in range(num_workers):
-    threading.Thread(target=worker, args=(i,)).start()
-host = ""
-port = 12345
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((host, port))
-s.listen(5)
-while True:
-    client_socket, addr = s.accept()
-    url = client_socket.recv(1024).decode()
-    master_queue.put((url, client_socket))
+        for i in range(self.num_workers):
+            t = threading.Thread(target=self.worker_loop)
+            t.start()
+            self.worker_threads.append(t)
+
+        while True:
+            client_socket, addr = self.socket.accept()
+            print(f"Accepted connection from {addr}")
+            url = client_socket.recv(1024).decode()
+            print(f"Received URL: {url}")
+            self.dispatch_work(url, client_socket)
+
+    def dispatch_work(self, url, client_socket):
+        self.task_queue.put((url, client_socket))
+
+    def worker_loop(self):
+        while True:
+            url, client_socket = self.task_queue.get()
+            try:
+                response = urllib.request.urlopen(url)
+                data = response.read().decode('utf-8')
+                words = data.split()
+                counter = Counter(words)
+                top_k_words = dict(counter.most_common(self.top_k))
+                client_socket.send(json.dumps(top_k_words).encode('utf-8'))
+            except Exception as e:
+                print(f"Error processing URL {url}: {str(e)}")
+            finally:
+                client_socket.close()
+                self.url_counter += 1
+                print(f"Processed {self.url_counter} URLs")
+
+                
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Master-worker server for processing client requests')
+    parser.add_argument('-w', '--workers', type=int, default=10, help='Number of worker threads (default 10)')
+    parser.add_argument('-k', '--top_k', type=int, default=7, help='Top K most common words to return (default 7)')
+    args = parser.parse_args()
+    server = MasterWorkerServer('localhost', 8000, args.workers, args.top_k)
+    server.start()
+
