c0c67da2db67089181e42b61f5e2f2febfa43240
Jimsgood <113968960+DmitriiKolesnikov@users.noreply.github.com>
2023-04-27 11:11:36 +0300

    Update Client.py

--- 06/Client.py
+++ 06/Client.py
@@ -1,18 +1,38 @@
 import socket
+import threading
+import json
+import argparse
 
-BUFFER_SIZE = 4096
+def process_url(url):
+    try:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect(('localhost', 8000))
+        client_socket.send(url.encode('utf-8'))
+        response = client_socket.recv(1024).decode('utf-8')
+        print(f"{url}: {response}")
+    except Exception as e:
+        print(f"Error processing URL {url}: {str(e)}")
+    finally:
+        client_socket.close()
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Client for sending requests to master-worker server')
+    parser.add_argument('urls_file', type=str, help='File containing URL list')
+    parser.add_argument('-m', '--threads', type=int, default=1, help='Number of threads (default 1)')
+    args = parser.parse_args()
 
-def get_top_words(url):
-    host = '127.0.0.1'
-    port = 65432
+    with open(args.urls_file) as f:
+        urls = f.readlines()
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((host, port))
-        s.sendall(url.encode())
-        data = s.recv(BUFFER_SIZE).decode()
-        return data
+    urls = [url.strip() for url in urls]
 
-
-if __name__ == "__main__":
-    print("Client")
+    for i in range(0, len(urls), args.threads):
+        threads = []
+        for j in range(args.threads):
+            if i + j < len(urls):
+                url = urls[i+j]
+                t = threading.Thread(target=process_url, args=(url,))
+                t.start()
+                threads.append(t)
+        for t in threads:
+            t.join()
