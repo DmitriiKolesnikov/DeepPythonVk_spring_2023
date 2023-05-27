f0bbc3a17328d92104e277a8e20203aa3381039f
Jimsgood <113968960+DmitriiKolesnikov@users.noreply.github.com>
2023-05-27 23:11:32 +0300

    Update Servert_test.py

--- 06/Servert_test.py
+++ 06/Servert_test.py
@@ -1,40 +1,149 @@
 
-import unittest
 from Server import *
+import asyncio
+import json
+import time
+import operator
+import re
+import unittest
+import urllib.parse
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from multiprocessing import Process
+
+from aiohttp import ClientSession
+
+from server import WordCountServer
+from client import RequestException, fetch_url, process_url, handle_responses
 
 
 class TestServer(unittest.TestCase):
 
-  def test_dispatch_work():
-      server = MasterWorkerServer('localhost', 8000, 10, 7)
-      client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      client_socket.connect(('localhost', 8000))
-      url = 'https://ru.wikipedia.org/wiki/Baba_Yaga'
-      server.dispatch_work(url, client_socket)
-      assert server.task_queue.get() == (url, client_socket)
+    def setUp(self):
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(None)
 
-  def test_worker_loop():
-      server = MasterWorkerServer('localhost', 8000, 10, 7)
-      url = 'https://ru.wikipedia.org/wiki/Baba_Yaga'
-      client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      server.task_queue.put((url, client_socket))
-      server.worker_loop()
-      response = client_socket.recv(1024).decode('utf-8')
-      assert isinstance(json.loads(response), dict)
+    def tearDown(self):
+        self.loop.close()
 
-  def test_start():
-      server = MasterWorkerServer('localhost', 8000, 10, 7)
-      t = threading.Thread(target=server.start)
-      t.start()
-      time.sleep(1)
-      client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      client_socket.connect(('localhost', 8000))
-      url = 'http://example.com'
-      client_socket.send(url.encode('utf-8'))
-      response = client_socket.recv(1024).decode('utf-8')
-      assert isinstance(json.loads(response), dict)
-      
-      
-if __name__ = "__main__":
+    def test_serve(self):
+        test_server = WordCountServer(loop=self.loop, listen='127.0.0.1', port=8080)
+        self.loop.run_until_complete(test_server.serve())
+        test_server.close()
+
+    def test_handle_request(self):
+
+        class MockRequestHandler(BaseHTTPRequestHandler):
+            protocol_version = 'HTTP/1.1'
+
+            def do_POST(self):
+
+                length = int(self.headers.get('content-length'))
+                data = self.rfile.read(length)
+                url = data.decode().strip()
+
+                async def mock_fetch_url(url):
+                    return 'test text\n'
+
+                result = self.server.process_url(url, mock_fetch_url)
+                json_result = json.dumps(result)
+                self.send_response(200)
+                self.send_header('Content-Type', 'application/json')
+                self.send_header('Content-Length', len(json_result))
+                self.end_headers()
+
+                self.wfile.write(json_result.encode())
+
+        server = HTTPServer(('127.0.0.1', 8080), MockRequestHandler)
+        server.process_url = lambda url, fetch_url: {"the": 1, "quick": 1, "brown": 1, "fox": 1}
+        process = Process(target=server.serve_forever)
+        process.start()
+
+        # Wait for the server to start listening
+        while True:
+            try:
+                response = urllib.request.urlopen('http://127.0.0.1:8080')
+                break
+            except urllib.error.URLError:
+                time.sleep(0.1)
+
+        with self.subTest("Valid URL"):
+            url = 'http://example.com/text.txt'
+            req = urllib.request.Request('http://127.0.0.1:8080', data=url.encode())
+            response = urllib.request.urlopen(req)
+            self.assertEqual(response.status, 200)
+            self.assertEqual(json.loads(response.read()), {"the": 1, "quick": 1, "brown": 1, "fox": 1})
+
+        with self.subTest("Invalid URL"):
+            url = 'not_a_url'
+            req = urllib.request.Request('http://127.0.0.1:8080', data=url.encode())
+            with self.assertRaises(HTTPError):
+                urllib.request.urlopen(req)
+
+        with self.subTest("Empty URL"):
+            url = ''
+            req = urllib.request.Request('http://127.0.0.1:8080', data=url.encode())
+            with self.assertRaises(HTTPError):
+                urllib.request.urlopen(req)
+
+        server.shutdown()
+        process.join(1)
+
+        if process.is_alive():
+            process.terminate()
+            process.join()
+
+    def test_handle_request_timeout(self):
+
+        class MockRequestHandler(BaseHTTPRequestHandler):
+            protocol_version = 'HTTP/1.1'
+
+            def do_POST(self):
+
+                length = int(self.headers.get('content-length'))
+                data = self.rfile.read(length)
+                url = data.decode().strip()
+
+                async def mock_fetch_url(url):
+                    time.sleep(0.5)
+                    return 'test text\n'
+
+                result = self.server.process_url(
+                    url,
+                    mock_fetch_url,
+                    request_timeout=0.1
+                )
+                json_result = json.dumps(result)
+                self.send_response(200)
+                self.send_header('Content-Type', 'application/json')
+                self.send_header('Content-Length', len(json_result))
+                self.end_headers()
+
+                self.wfile.write(json_result.encode())
+
+        server = HTTPServer(('127.0.0.1', 8080), MockRequestHandler)
+        process = Process(target=server.serve_forever)
+        process.start()
+
+        # Wait for the server to start listening
+        while True:
+            try:
+                response = urllib.request.urlopen('http://127.0.0.1:8080')
+                break
+            except urllib.error.URLError:
+                time.sleep(0.1)
+
+        with self.subTest("Timeout"):
+            url = 'http://example.com/text.txt'
+            req = urllib.request.Request('http://127.0.0.1:8080', data=url.encode())
+            with self.assertRaises(HTTPError):
+                urllib.request.urlopen(req)
+
+        server.shutdown()
+        process.join(1)
+
+        if process.is_alive():
+            process.terminate()
+            process.join()
+
+if __name__ == "__main__":
   unittest.main()
-
