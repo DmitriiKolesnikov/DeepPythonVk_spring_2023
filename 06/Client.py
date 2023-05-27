9496293180dbbf0f7098c90fcafe2b6fb4e1edaa
Jimsgood <113968960+DmitriiKolesnikov@users.noreply.github.com>
2023-05-27 23:05:45 +0300

    Update Client.py

--- 06/Client.py
+++ 06/Client.py
@@ -1,38 +1,55 @@
-import socket
-import threading
-import json
 import argparse
+import asyncio
+import json
+import operator
 
-def process_url(url):
-    try:
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect(('localhost', 8000))
-        client_socket.send(url.encode('utf-8'))
-        response = client_socket.recv(1024).decode('utf-8')
-        print(f"{url}: {response}")
-    except Exception as e:
-        print(f"Error processing URL {url}: {str(e)}")
-    finally:
-        client_socket.close()
+import aiohttp
+
+
+async def fetch_url(session, url):
+    async with session.get(url) as response:
+        return await response.text()
+
+async def process_url(session, url, results_queue):
+    text = await fetch_url(session, url)
+    words = re.findall(r'\w+', text.lower())
+    word_counts = {}
+    for word in words:
+        if word in word_counts:
+            word_counts[word] += 1
+        else:
+            word_counts[word] = 1
+    sorted_word_counts = sorted(word_counts.items(), key=operator.itemgetter(1), reverse=True)[:top_k]
+    result = {word: count for word, count in sorted_word_counts}
+    results_queue.put((url, result))
+
+async def handle_responses(results_queue):
+    while True:
+        url, result = await results_queue.get()
+        print(f'{url}: {result}')
+        results_queue.task_done()
+
+async def main(urls_file, num_tasks):
+    async with aiohttp.ClientSession() as session:
+        results_queue = asyncio.Queue()
+        urls = []
+        with open(urls_file) as f:
+            urls = f.read().splitlines()
+        tasks = []
+        for url in urls:
+            task = asyncio.ensure_future(process_url(session, url, results_queue))
+            tasks.append(task)
+
+        results_task = asyncio.ensure_future(handle_responses(results_queue))
+        await asyncio.gather(*tasks)
+        await results_queue.join()
+        results_task.cancel()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Client for sending requests to master-worker server')
-    parser.add_argument('urls_file', type=str, help='File containing URL list')
-    parser.add_argument('-m', '--threads', type=int, default=1, help='Number of threads (default 1)')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('num_tasks', type=int, help='Number of concurrent tasks')
+    parser.add_argument('urls_file', type=str, help='Path to file with URLs')
     args = parser.parse_args()
 
-    with open(args.urls_file) as f:
-        urls = f.readlines()
-
-    urls = [url.strip() for url in urls]
-
-    for i in range(0, len(urls), args.threads):
-        threads = []
-        for j in range(args.threads):
-            if i + j < len(urls):
-                url = urls[i+j]
-                t = threading.Thread(target=process_url, args=(url,))
-                t.start()
-                threads.append(t)
-        for t in threads:
-            t.join()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(args.urls_file, args.num_tasks))
