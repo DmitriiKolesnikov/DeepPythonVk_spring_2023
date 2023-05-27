e69e49b54fba4873edc6abbe14c40ec448779ba3
Jimsgood <113968960+DmitriiKolesnikov@users.noreply.github.com>
2023-05-27 20:03:37 +0300

    Update and rename LRU-cache.py to LRU_cache.py

---
+++ 05/LRU_cache.py
@@ -0,0 +1,54 @@
+
+class Node:
+
+    def __init__(self, key=None, val=None):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+
+class LRUCache:
+
+    def __init__(self, limit):
+        self.limit = limit
+        self.cache = {}
+        self.head = Node()
+        self.tail = Node()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def get(self, key):
+        if key not in self.cache:
+            return None
+        node = self.cache[key]
+        self._remove(node)
+        self._add(node)
+        return node.val
+
+    def set(self, key, value):
+        if key in self.cache:
+            self._remove(self.cache[key])
+        node = Node(key, value)
+        self.cache[key] = node
+        self._add(node)
+        if len(self.cache) > self.limit:
+            node = self.head.next
+            self._remove(node)
+            del self.cache[node.key]
+
+    def _remove(self, node):
+        prev = node.prev
+        next = node.next
+        prev.next = next
+        next.prev = prev
+
+    def _add(self, node):
+        prev = self.tail.prev
+        prev.next = node
+        node.prev = prev
+        node.next = self.tail
+        self.tail.prev = node
+
+
+
