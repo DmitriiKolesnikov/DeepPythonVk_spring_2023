717c0f329ddd049662b85f964c39de66dd1e402e
Jimsgood <113968960+DmitriiKolesnikov@users.noreply.github.com>
2023-05-27 22:52:46 +0300

    Update Test_Script.py

--- 07/Test_Script.py
+++ 07/Test_Script.py
@@ -1,22 +1,42 @@
 import unittest
+from unittest.mock import MagicMock, patch
 import asyncio
-from unittest.mock import patch, MagicMock
-from Script import fetch_urls
+import aiohttp
+
+from Script import process_url, fetch_urls
 
 
-class TestFetchUrls(unittest.TestCase):
-    @patch('Script.aiohttp.ClientSession')
-    def test_fetch_urls(self, mock_session):
-        urls_file = 'urls.txt'
-        concurrent_requests = 10
-        mock_response = MagicMock()
-        mock_response.text.return_value = '<html></html>'
-        mock_session.return_value.__aenter__ = MagicMock(return_value=mock_session.return_value)
-        mock_session.return_value.get.return_value = asyncio.Future()
-        mock_session.return_value.get.return_value.set_result(mock_response)
-        loop = asyncio.get_event_loop()
-        responses = loop.run_until_complete(fetch_urls(urls_file, concurrent_requests))
-        self.assertEqual(len(responses), 1)
-        self.assertEqual(responses[0], '<html></html>')
+class TestProcessUrl(unittest.IsolatedAsyncioTestCase):
+    async def test_process_url(self):
+        with patch("aiohttp.ClientSession.get") as mock:
+            url = "http://test.com/"
+            response_mock = MagicMock()
+
+            response_mock.text.return_value = "test"
+            mock.return_value.__aenter__.return_value = response_mock
+
+            session = aiohttp.ClientSession()
+            response = await process_url(session, url)
+            self.assertEqual(response, "test")
+            mock.assert_called_once()
+            response_mock.text.assert_called_once()
 
 
+class TestFetchUrls(unittest.IsolatedAsyncioTestCase):
+    async def test_fetch_urls(self):
+        with patch("code.process_url") as mock:
+            urls_file = "/path/to/urls/file",
+            concurrent_requests = 5,
+            response_mock = MagicMock()
+
+            response_mock.text.return_value = "test"
+            mock.return_value = response_mock
+
+            session = aiohttp.ClientSession()
+            result = await fetch_urls(urls_file, concurrent_requests)
+            mock.assert_called()
+            self.assertEqual(len(result), 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
