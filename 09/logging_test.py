dcdab3df057e55184ad47d76e4218e2dac0c6ee7
Jimsgood <113968960+DmitriiKolesnikov@users.noreply.github.com>
2023-05-28 21:39:03 +0300

    Update logging_test.py

--- 09/logging_test.py
+++ 09/logging_test.py
@@ -1,49 +1,34 @@
 import unittest
 from logging import LRUCache
 
+
 class TestLRUCache(unittest.TestCase):
-    def setUp(self):
-        self.cache = LRUCache(3)
 
-    def test_get_existing_key(self):
-        self.cache.set(1, "one")
-        self.assertEqual(self.cache.get(1), "one")
+    def test_get_and_set(self):
+        lru_cache = LRUCache(2)
+        lru_cache.set(1, "one")
+        lru_cache.set(2, "two")
+        self.assertEqual(lru_cache.get(1), "one")
+        lru_cache.set(3, "three")
+        self.assertEqual(lru_cache.get(2), None)
+        lru_cache.set(4, "four")
+        self.assertEqual(lru_cache.get(1), None)
+        self.assertEqual(lru_cache.get(3), "three")
+        self.assertEqual(lru_cache.get(4), "four")
 
-    def test_get_missing_key(self):
-        self.assertEqual(self.cache.get(1), "Key not found")
+    def test_capacity(self):
+        lru_cache = LRUCache(2)
+        lru_cache.set(1, "one")
+        lru_cache.set(2, "two")
+        lru_cache.set(3, "three")
+        self.assertEqual(lru_cache.get(1), None)
+        self.assertEqual(lru_cache.get(2), "two")
+        self.assertEqual(lru_cache.get(3), "three")
+        lru_cache.set(4, "four")
+        self.assertEqual(lru_cache.get(2), None)
+        self.assertEqual(lru_cache.get(3), "three")
+        self.assertEqual(lru_cache.get(4), "four")
 
-    def test_set_existing_key(self):
-        self.cache.set(1, "one")
-        self.cache.set(1, "new_one")
-        self.assertEqual(self.cache.get(1), "new_one")
-
-    def test_set_missing_key(self):
-        self.cache.set(1, "one")
-        self.cache.set(2, "two")
-        self.cache.set(3, "three")
-        self.cache.set(4, "four")
-        self.assertEqual(self.cache.get(1), "Key not found")
-        self.assertEqual(self.cache.get(2), "two")
-        self.assertEqual(self.cache.get(3), "three")
-        self.assertEqual(self.cache.get(4), "four")
-
-    def test_peek(self):
-        self.cache.set(1, "one")
-        self.assertEqual(len(self.cache.cache), 1)
-        self.assertEqual(len(self.cache.lru_list), 1)
-        self.assertEqual(self.cache.lru_list[-1], 1)
-        self.cache.set(2, "two")
-        self.assertEqual(len(self.cache.cache), 2)
-        self.assertEqual(len(self.cache.lru_list), 2)
-        self.assertEqual(self.cache.lru_list[-1], 2)
-        self.cache.set(3, "three")
-        self.assertEqual(len(self.cache.cache), 3)
-        self.assertEqual(len(self.cache.lru_list), 3)
-        self.assertEqual(self.cache.lru_list[-1], 3)
-        self.cache.get(1)
-        self.assertEqual(len(self.cache.cache), 3)
-        self.assertEqual(len(self.cache.lru_list), 3)
-        self.assertEqual(self.cache.lru_list[-1], 1)
 
 if __name__ == '__main__':
     unittest.main()
